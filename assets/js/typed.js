ba192f1ba719fd1a82eb252e16af0365c23e1d32
Burin Techama <benmore1992@gmail.com>
2020-01-04 18:25:46 +0700

    add new theme

---
+++ assets/js/typed.js
@@ -0,0 +1,59 @@
+(function($) {
+    "use strict";
+
+    /*************************
+     typed js start
+     *************************/
+    $(function () {
+        $(".typed").typed({
+            strings: ["Unice"],
+            stringsElement: null,
+            typeSpeed: 120,
+            backSpeed: 30,
+            showCursor: false,
+            loop: true,
+            cursorChar: "|",
+            attr: null,
+            contentType: 'html',
+            callback: function () {
+            },
+            preStringTyped: function () {
+            },
+            onStringTyped: function () {
+            },
+            resetCallback: function () {
+            }
+        });
+    });
+    /*************************
+     typed js end
+     *************************/
+    /*************************
+     typed js start
+     *************************/
+    $(function () {
+        $(".typed-resume").typed({
+            strings: ["objective-c, json, html, css"],
+            stringsElement: null,
+            typeSpeed: 120,
+            backSpeed: 30,
+            showCursor: false,
+            loop: true,
+            cursorChar: "|",
+            attr: null,
+            contentType: 'html',
+            callback: function () {
+            },
+            preStringTyped: function () {
+            },
+            onStringTyped: function () {
+            },
+            resetCallback: function () {
+            }
+        });
+    });
+})(jQuery);
+
+/*************************
+ typed js end
+ *************************/
